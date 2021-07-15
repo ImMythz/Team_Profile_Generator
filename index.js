495780088c77382e5ee042fab5afa8ffb68a7d93
Nate Worley <immythz@gmail.com>
2021-07-14 19:23:53 -0500

    created initial set of questions and empty team array to push members to

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const util = require('util')
 
+let team = []
+
+const initPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your managers name?'
+        },
+        {
+            type: 'input', 
+            name: 'managerId',
+            message: 'What is thier employee id?'
+        }, 
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your managers email?'
+        }, 
+        {
+            type: 'input',
+            name: 'number',
+            message: 'What is their office number?'
+        },
+    ])
+    .then((answers) => {
+
+    })
+ 
+}
+
+
