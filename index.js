5c558ac6a660f8386bc977decdb30aa2081c559b
Nate Worley <immythz@gmail.com>
2021-07-14 20:12:10 -0500

    added logic to choose between adding a member or not. Setup file writing function

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const util = require('util')
+const generateHTML = require('./src/generateHTML')
 
 let team = []
 
-const initPrompt = () => {
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const initalPrompt = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -26,11 +30,34 @@ const initPrompt = () => {
             name: 'number',
             message: 'What is their office number?'
         },
+        {
+            type: 'list',
+            name: 'addMemeber',
+            message: 'Would you like to add a team member?',
+            choices: ['Engineer', 'Intern', 'I do not wish to add any more team members']
+        }
     ])
     .then((answers) => {
-
+        switch(answers.addMember) {
+            case 'Engineer':
+                engineerPrompts()
+                break;
+            case 'Intern':
+                internPrompts()
+                break;
+            default:
+                writeFileAsync('index.html', generateHTML(answers))
+        }
     })
- 
+    .catch((err) => console.log(err))
 }
+
+
+const init = () => {
+    initalPrompt()
+    .catch((err) => console.error(err));
+};
+  
+init();
 
 
